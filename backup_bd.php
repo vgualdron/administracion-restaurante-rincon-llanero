4b8b50f014687ce627673c54f5c7b52541ac3998
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2022-11-15 10:36:35 -0500

    add backup file

---
+++ backup_bd.php
@@ -0,0 +1,26 @@
+<?php
+//Datos de la base de datos
+$mysqlDatabaseName ='pinchetas_general';
+$mysqlUserName ='root';
+$mysqlPassword ='';
+$mysqlHostName ='localhost';
+$mysqlExportPath ='C:\Users\vgualdron\Desktop\db_test_pinchetas_backup.sql';
+
+// Backup con mysqldump
+$command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ' .$mysqlExportPath;
+$output=array();
+exec("($command)2>&1",$output,$worked);
+echo print_r($output);
+echo $worked;
+switch($worked){
+case 0:
+echo 'La base de datos <b>' .$mysqlDatabaseName .'</b> se ha almacenado correctamente en la siguiente ruta '.getcwd().'/' .$mysqlExportPath .'</b>';
+break;
+case 1:
+echo 'Se ha producido un error al exportar <b>' .$mysqlDatabaseName .'</b> a '.getcwd().'/' .$mysqlExportPath .'</b>';
+break;
+case 2:
+echo 'Se ha producido un error de exportación, compruebe la siguiente información: <br/><br/><table><tr><td>Nombre de la base de datos:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>Nombre de usuario MySQL:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>Contraseña MySQL:</td><td><b>NOTSHOWN</b></td></tr><tr><td>Nombre de host MySQL:</td><td><b>' .$mysqlHostName .'</b></td></tr></table>';
+break;
+}
+?>
